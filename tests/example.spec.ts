0a8b94097e1a8786282903b6525e4056525b54ee
“namra19” <namra.anjum1919@gmail.com>
2026-04-03 08:39:28 +0200

    added logs

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-  page.pause()
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Single Page Application/);
+  console.log(await page.title());
 });
 
 // test('get started link', async ({ page }) => {
