22f8ba0a842a197c312788a8e7afff40180884ef
“namra19” <namra.anjum1919@gmail.com>
2026-04-05 12:28:44 +0200

    # This is a combination of 2 commits. # This is the 1st commit message:
    updated tests
    
    # This is the commit message #2:
    
    updated login tests

---
+++ tests/auth/login.spec.ts
@@ -0,0 +1,48 @@
+import { LoginPage } from '../../page-objects/LoginPage';
+import { test, expect } from '../../fixtures/auth.fixture';
+import { URLs } from '../../utils/urls';
+
+
+test.describe('Login Tests', () => {
+    let loginPage: LoginPage;
+
+    test.beforeEach(async ({ context, page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.navigate();
+    });
+
+    // Verify website is loaded successfully 
+    test('Verify home page opens successfully', async ({ page }) => {
+        await loginPage.verifyTitle('Single Page Application');
+    });
+
+    //Valid login - admin
+    test('Admin Login with valid credentials', async ({ loginAs, page }) => {
+        await loginAs('adminLogin');
+        await expect(page).toHaveURL(URLs.baseURL)
+    });
+
+    // Valid login - user 1
+    test('User1 Login with valid credentials', async ({ loginAs }) => {
+        await loginAs('user1Login');
+    });
+
+    // Valid login - user 2
+    test('User2 Login with valid credentials', async ({ loginAs }) => {
+        await loginAs('user2Login');
+    });
+
+    //Negative tests for login
+    test('Login with invalid credentials', async ({ loginAs }) => {
+        await loginAs('invalidEmail');
+    });
+
+    test('Login with empty credentials', async ({ loginAs }) => {
+        await loginAs('emptyCredentials');
+    });
+
+    test('Login with credentials of invalid format', async ({ loginAs }) => {
+        await loginAs('invalidFormat');
+    });
+
+});
