1aa25a5e37064b03b1622c1966f1811c15ca81fd
“namra19” <namra.anjum1919@gmail.com>
2026-04-05 19:01:28 +0200

    Added test for signout

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -3,13 +3,24 @@ import { expect, Locator, Page } from '@playwright/test';
 export class HomePage {
     readonly page: Page;
     readonly contentSection: Locator;
+    readonly userIcon: Locator;
+    readonly signOut: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.contentSection = page.locator('text=Lorem ipsum egestas');
+        this.userIcon = page.locator('#user');
+        this.signOut = page.getByText('Sign Out')
     }
     //Verify user is logged in
     async verifyUserIsLoggedIn() {
         await expect(this.contentSection).toBeVisible();
     }
+
+    //Click SignOut
+    async clickSignOut() {
+        await this.userIcon.click();
+        await this.signOut.click();
+    }
+
 }
